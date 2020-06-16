284a5874fcc4a98682e7643e2a8631079194b468
Ana Luiza <avelar.analuiza@gmail.com>
2020-06-16 19:57:52 -0300

    ENG-574 Draft: Draft config_test.go, ignore test coverage html file and change http mock

--- cyral/config_test.go
+++ cyral/config_test.go
@@ -1,137 +1,103 @@
+// To generate detailed test cover:
+// go test -v -coverprofile cover.out && go tool cover -html=cover.out
+
 package cyral
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"strings"
 	"testing"
+	//"net/http/httptest"
 )
 
-func TestEnvVarAuth0ClientID(t *testing.T) {
-	c := Config{}
+func invalidHTTPrequest() error {
+
+	c := Config{Auth0Domain: "Auth0DomainValue", Auth0Audience: "Auth0AudienceValue"}
+	os.Setenv("AUTH0_CLIENT_ID", "Auth0ClientIDvalue")
+	os.Setenv("AUTH0_CLIENT_SECRET", "Auth0ClientIDsecret")
+
+	// readTokenInfo() call: will always be correct in type matching
+
+	// url assignment: will never fail, always receives right parameter type
+	// and imported fuction call fmt.Sprintf already tested
+
+	// audienceUrl assignment: will never fail,always receives right parameter
+	// type and imported fuction call fmt.Sprintf already tested
+
+	// tokenReq assignment: will never fail: always correct type and already
+	// checked values lecture
+
+	// payloadBytes assignment: Encoding to json call over tokenReq never fails.
+	// Since tokenReq is always composed only by strings, and also as its values
+	// will never be cyclic, this function will never fail.
+
+	// Creating a http request using the paramaters will never fail.
+	// http.NewRequest() method wraps, using argument data, a http request,
+	// without sending it. It Fails when a non-valid method is entered, or
+	// it receives a non valid body type. Since in this code it will always
+	// receive a valid method (always http.MethodPost), and a body of a
+	// correct stream type, and also always receives a string as url, this
+	// method will never have problems creating a request using argument data
+	// received.
+
+	// statement req.Header.Add will never fail since it always receives right
+	// arguments for execution
+
 	if _, err := c.Client(); err != nil {
-		if _, err2 := c.getEnv("AUTH0_CLIENT_ID"); err.Error() != err2.Error() {
-			t.Error("Expected error in env var: AUTH0_CLIENT_ID")
-		}
-	}
-}
-func TestEnvVarAuth0ClientSecret(t *testing.T) {
-	c := Config{}
-	os.Setenv("AUTH0_CLIENT_ID", "bla")
-	if _, err := c.Client(); err != nil {
-		if _, err2 := c.getEnv("AUTH0_CLIENT_SECRET"); err.Error() != err2.Error() {
-			t.Error("Expected error in env var: AUTH0_CLIENT_SECRET")
-		}
-	}
-}
-
-// FALTA: MOCK CLIENT (E ONDE ELE APARECE)
-
-/*
-//rascunho
-type HTTPClient interface {
-	Do(req *http.Request)(*http.Response, error)
-}
-
-type MockClient http.Client
-
-func (mock *MockClient) Do(req *http.Request) (respAddr *http.Response, error) {
-	auth0Token := auth0TokenResponse{AccessToken: "", TokenType:""}
-	body, err := json.Marshal(auth0Token)
-
-	respAddr := &http.Response{}
-	// erro ?
-	//tratar req: erro ou não
-	respAddr := New(http.Response)
-	*respAddr = http.Response{}
-	// erro = ?
-	json.Marshal()
-
-}
-//fimrascunho
-//**************** implementar a interface Do mock
-*/
-
-func (confAddr *Config) discoverTokenError(clientID, clientSecret string) error {
-	url := fmt.Sprintf("https://%s/oauth/token", confAddr.Auth0Domain)
-	audienceURL := fmt.Sprintf("https://%s", confAddr.Auth0Audience)
-	tokenReq := auth0TokenRequest{
-		Audience:     audienceURL,
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		GrantType:    "client_credentials",
-	}
-
-	payloadBytes, err := json.Marshal(tokenReq)
-	if err != nil {
-		return fmt.Errorf("Token error, failed to encode readToken payload: %v.\n", err)
-	}
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(string(payloadBytes)))
-	if err != nil {
-		return fmt.Errorf("Token error, unable to create auth0 request; err: %v", err)
-	}
-	req.Header.Add("content-type", "application/json")
-	//***********************
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("Token error, unable execute auth0 request; err: %v", err)
-	}
-	defer res.Body.Close() ///*****************
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("Token error, unable to read data from request body; err: %v", err)
-	}
-	token := auth0TokenResponse{}
-	err = json.Unmarshal(body, &token)
-	//if err != nil {
-	return fmt.Errorf("Token error, unable to get access token from json; err: %v", err)
-	//}
-}
-
-func (confAddr *Config) checkEnvVar(envVar string) (string, error) {
-	varValue, err := confAddr.getEnv(envVar)
-	if err != nil {
-		return "", err
-	}
-	return varValue, nil
-}
-
-func (confAddr *Config) traceProblem() error {
-	auth0ClientID, err := confAddr.checkEnvVar("AUTH0_CLIENT_ID")
-	if err != nil {
 		return err
 	}
-	auth0ClientSecret, err := confAddr.checkEnvVar("AUTH0_CLIENT_SECRET")
-	if err != nil {
-		return err
+	return nil
+}
+
+func absentEnvVarAuth0ClientID() error {
+	c := Config{}
+
+	// Remove env var AUTH0_CLIENT_ID, if present
+	if _, err := c.getEnv("AUTH0_CLIENT_ID"); err == nil {
+		os.Unsetenv("AUTH0_CLIENT_ID")
 	}
-	return confAddr.discoverTokenError(auth0ClientID, auth0ClientSecret)
+
+	_, err2 := c.Client()
+
+	if _, err := c.getEnv("AUTH0_CLIENT_ID"); err2.Error() != err.Error() {
+		return fmt.Errorf("Unexpected behavior in Client() function.\n")
+	}
+	return nil
+}
+
+func absentEnvVarAuth0ClientSecret() error {
+	c := Config{}
+	os.Setenv("AUTH0_CLIENT_ID", "Auth0ClientIDvalue")
+
+	// Remove env var AUTH0_CLIENT_SECRET, if present
+	if _, err := c.getEnv("AUTH0_CLIENT_SECRET"); err == nil {
+		os.Unsetenv("AUTH0_CLIENT_SECRET")
+	}
+
+	_, err2 := c.Client()
+
+	if _, err := c.getEnv("AUTH0_CLIENT_SECRET"); err2.Error() != err.Error() {
+		return fmt.Errorf("Unexpected behavior in Client() function.\n")
+	}
+	return nil
 }
 
 func TestClient(t *testing.T) {
 
-	mockConfig := Config{}
-
-	/* //teste
-	t.Logf("For test purposes:\n")
-	t.Logf("Auth0Domain: %s\n", mockConfig.Auth0Domain)
-	t.Logf("Auth0Audience: %s\n", mockConfig.Auth0Audience)
-	t.Logf("controlPlane: %s\n", mockConfig.controlPlane)
-	t.Logf("controlPlaneAPIPort: %d\n", mockConfig.controlPlaneAPIPort)
-	t.Logf("terraformVersion: %s\n", mockConfig.terraformVersion)
-	//fimteste */
-
-	_, err := (&mockConfig).Client()
-
-	if err != nil {
-		// ARRUMAR PARA MSGS SIGNIFICATIVAS!!!
-		// exemplo: "erro ao inicialiazar abjshas. + msg erro comum."
-		//t.Log ( (&mockConfig).traceProblem() )
-		t.Error((&mockConfig).traceProblem())
+	if err := absentEnvVarAuth0ClientID(); err != nil {
+		t.Error(err)
 	}
+	if err := absentEnvVarAuth0ClientSecret(); err != nil {
+		t.Error(err)
+	}
+
+	if err := invalidHTTPrequest(); err != nil {
+		//t.Error(err)
+		t.Log(err)
+	}
+
 }
 
-//func Test
+// getEnv() function: as defined, will never fail, since it is only
+// another way to print os.LookupEnv() results, and os.LookupEnv() is already
+// officialy tested.
