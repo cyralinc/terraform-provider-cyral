aacabf1578d783090a6f24286a9a91c1e89ecca7
Wilson de Carvalho <wjunior@cyral.com>
2020-06-18 14:28:49 -0300

    ENG-574 Disable CA validation

--- cyral/config_test.go
+++ cyral/config_test.go
@@ -4,6 +4,7 @@
 package cyral
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -12,6 +13,10 @@ import (
 )
 
 func invalidHTTPrequest() error {
+	// Disables certificate authority validation in order to
+	// successfully mock https requests
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
 	os.Setenv("AUTH0_CLIENT_ID", "Auth0ClientIDvalue")
 	os.Setenv("AUTH0_CLIENT_SECRET", "Auth0ClientIDsecret")
 
@@ -20,50 +25,12 @@ func invalidHTTPrequest() error {
 	}))
 
 	c := Config{Auth0Domain: ts.URL[8:len(ts.URL)], Auth0Audience: "cyral"}
-	// Pass TLS certificate "cert.pem", "key.pem"
-	// ts.Config.TLSConfig.Certificates
-	// See: https://golang.org/pkg/net/http/httptest/#Server.Certificate
 	ts.URL = ts.URL + "/oauth/token"
 	defer ts.Close()
 
 	_, err := c.Client()
 
 	return err
-
-	//_, err := c.Client()
-	//return err
-
-	// readTokenInfo() call: will always be correct in type matching
-
-	// url assignment: will never fail, always receives right parameter type
-	// and imported fuction call fmt.Sprintf already tested
-
-	// audienceUrl assignment: will never fail,always receives right parameter
-	// type and imported fuction call fmt.Sprintf already tested
-
-	// tokenReq assignment: will never fail: always correct type and already
-	// checked values lecture
-
-	// payloadBytes assignment: Encoding to json call over tokenReq never fails.
-	// Since tokenReq is always composed only by strings, and also as its values
-	// will never be cyclic, this function will never fail.
-
-	// Creating a http request using the paramaters will never fail.
-	// http.NewRequest() method wraps, using argument data, a http request,
-	// without sending it. It Fails when a non-valid method is entered, or
-	// it receives a non valid body type. Since in this code it will always
-	// receive a valid method (always http.MethodPost), and a body of a
-	// correct stream type, and also always receives a string as url, this
-	// method will never have problems creating a request using argument data
-	// received.
-
-	// statement req.Header.Add will never fail since it always receives right
-	// arguments for execution
-
-	/*if _, err := c.Client(); err != nil {
-		return err
-	}
-	return nil*/
 }
 
 func absentEnvVarAuth0ClientID() error {
