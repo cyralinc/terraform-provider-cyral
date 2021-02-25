d6598db4a30f10725379169113ce9b2b2f6aa640
Wilson de Carvalho <wjunior@cyral.com>
2021-02-25 19:25:52 -0300

    Provider tests preparation

--- cyral/provider_test.go
+++ cyral/provider_test.go
@@ -1,13 +1,34 @@
 package cyral
 
-/*
 import (
-	"fmt"
-	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+var testAccProviders map[string]*schema.Provider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
+		"cyral": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+/*
 func absentEnvVarAuth0ClientID() error {
-	c := Config{}
+	c := NewClient("",
+		"Auth0ClientSecretValue",
+		"Auth0DomainValue",
+		"Auth0AudienceValue",
+		"ControlPlaneValue")
 
 	// Remove env var AUTH0_CLIENT_ID, if present
 	os.Unsetenv("AUTH0_CLIENT_ID")
