a0ae892b0f66bbf4c92bd47ea24f29f1b7346adb
lorenaleao <lleao@cyral.com>
2021-03-02 16:31:04 -0300

    Define a scaffold for the datamap resource

---
+++ cyral/resource_datamap.go
@@ -0,0 +1,43 @@
+package cyral
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceDatamap() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceDatamapCreate,
+		ReadContext:   resourceDatamapRead,
+		UpdateContext: resourceDatamapUpdate,
+		DeleteContext: resourceDatamapDelete,
+		Schema:        map[string]*schema.Schema{},
+	}
+}
+
+func resourceDatamapCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceDatamapRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceDatamapUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return resourceDatamapRead(ctx, d, m)
+}
+
+func resourceDatamapDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	return diags
+}
