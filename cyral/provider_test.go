1d642096f94297cd737ab2af7df7ccb807865051
Becca Petrin <beccapetrin@gmail.com>
2021-03-19 16:30:16 -0700

    Add test starter kit (#32)

--- cyral/provider_test.go
+++ cyral/provider_test.go
@@ -1,8 +1,20 @@
 package cyral
 
 import (
+	"os"
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+// Example value: "stable.dev.cyral.com"
+const EnvVarControlPlaneBaseURL = "TEST_TPC_CP_URL"
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
 
 var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
@@ -14,47 +26,8 @@ func init() {
 	}
 }
 
-/*func TestProvider(t *testing.T) {
-	if err := Provider().InternalValidate(); err != nil {
-		t.Fatalf("err: %s", err)
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv(EnvVarControlPlaneBaseURL); v == "" {
+		t.Fatalf("%q must be set for acceptance tests", EnvVarControlPlaneBaseURL)
 	}
-}*/
-
-/*
-func absentEnvVarAuth0ClientID() error {
-	c := NewClient("",
-		"Auth0ClientSecretValue",
-		"Auth0DomainValue",
-		"Auth0AudienceValue",
-		"ControlPlaneValue")
-
-	// Remove env var AUTH0_CLIENT_ID, if present
-	os.Unsetenv("AUTH0_CLIENT_ID")
-
-	_, err := c.Client()
-
-	if _, err2 := c.getEnv("AUTH0_CLIENT_ID"); err2.Error() != err.Error() {
-		return fmt.Errorf(
-			"unexpected behavior in Client() when AUTH0_CLIENT_ID is empty; err: %v",
-			err.Error())
-	}
-	return nil
 }
-
-func absentEnvVarAuth0ClientSecret() error {
-	c := Config{}
-	os.Setenv("AUTH0_CLIENT_ID", "Auth0ClientIDvalue")
-
-	// Remove env var AUTH0_CLIENT_SECRET, if present
-	os.Unsetenv("AUTH0_CLIENT_SECRET")
-
-	_, err := c.Client()
-
-	if _, err2 := c.getEnv("AUTH0_CLIENT_SECRET"); err2.Error() != err.Error() {
-		return fmt.Errorf(
-			"unexpected behavior in Client() when AUTH0_CLIENT_SECRET is empty; err: %v",
-			err.Error())
-	}
-	return nil
-}
-*/
