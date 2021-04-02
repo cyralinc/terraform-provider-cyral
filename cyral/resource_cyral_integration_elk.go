262ea897bcf193e826b1b2c789b76597f029291b
root <jsaffran@cyral.com>
2021-04-02 17:41:01 +0000

    refactoring elk integration

--- cyral/resource_cyral_integration_elk.go
+++ cyral/resource_cyral_integration_elk.go
@@ -1,19 +1,23 @@
 package cyral
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/cyralinc/terraform-provider-cyral/client"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 type CreateELKIntegrationResponse struct {
 	ID string `json:"ID"`
+}
+
+func (response CreateELKIntegrationResponse) WriteResourceData(d *schema.ResourceData) {
+	d.SetId(response.ID)
+}
+
+func (response *CreateELKIntegrationResponse) ReadResourceData(d *schema.ResourceData) {
+	response.ID = d.Id()
 }
 
 type ELKIntegrationData struct {
@@ -23,12 +27,63 @@ type ELKIntegrationData struct {
 	ESURL     string `json:"esUrl"`
 }
 
+func (data ELKIntegrationData) WriteResourceData(d *schema.ResourceData) {
+	d.Set("name", data.Name)
+	d.Set("kibana_url", data.KibanaURL)
+	d.Set("es_url", data.ESURL)
+}
+
+func (data *ELKIntegrationData) ReadResourceData(d *schema.ResourceData) {
+	data.ID = d.Id()
+	data.Name = d.Get("name").(string)
+	data.KibanaURL = d.Get("kibana_url").(string)
+	data.ESURL = d.Get("es_url").(string)
+}
+
+var CreateELKFunctionConfig = FunctionConfig{
+	Name:       "ELKResourceCreate",
+	HttpMethod: http.MethodPost,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/elk", c.ControlPlane)
+	},
+	ResourceData:       &ELKIntegrationData{},
+	ResponseData:       &CreateELKIntegrationResponse{},
+	ReadFunctionConfig: &ReadELKFunctionConfig,
+}
+
+var ReadELKFunctionConfig = FunctionConfig{
+	Name:       "ELKResourceRead",
+	HttpMethod: http.MethodGet,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
+	},
+	ResponseData: &ELKIntegrationData{},
+}
+
+var UpdateELKFunctionConfig = FunctionConfig{
+	Name:       "ELKResourceUpdate",
+	HttpMethod: http.MethodPut,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
+	},
+	ResourceData:       &ELKIntegrationData{},
+	ReadFunctionConfig: &ReadELKFunctionConfig,
+}
+
+var DeleteELKFunctionConfig = FunctionConfig{
+	Name:       "ELKResourceDelete",
+	HttpMethod: http.MethodDelete,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
+	},
+}
+
 func resourceIntegrationELK() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceIntegrationELKCreate,
-		ReadContext:   resourceIntegrationELKRead,
-		UpdateContext: resourceIntegrationELKUpdate,
-		DeleteContext: resourceIntegrationELKDelete,
+		CreateContext: CreateELKFunctionConfig.Create,
+		ReadContext:   ReadELKFunctionConfig.Read,
+		UpdateContext: UpdateELKFunctionConfig.Update,
+		DeleteContext: DeleteELKFunctionConfig.Delete,
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -47,99 +102,5 @@ func resourceIntegrationELK() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
-	}
-}
-
-func resourceIntegrationELKCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationELKCreate")
-	c := m.(*client.Client)
-
-	resourceData := getELKIntegrationDataFromResource(c, d)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/elk", c.ControlPlane)
-
-	body, err := c.DoRequest(url, http.MethodPost, resourceData)
-	if err != nil {
-		return createError("Unable to create integration", fmt.Sprintf("%v", err))
-	}
-
-	response := CreateELKIntegrationResponse{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return createError("Unable to unmarshall JSON", fmt.Sprintf("%v", err))
-	}
-	log.Printf("[DEBUG] Response body (unmarshalled): %#v", response)
-
-	d.SetId(response.ID)
-
-	log.Printf("[DEBUG] End resourceIntegrationELKCreate")
-
-	return resourceIntegrationELKRead(ctx, d, m)
-}
-
-func resourceIntegrationELKRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationELKRead")
-	c := m.(*client.Client)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
-
-	body, err := c.DoRequest(url, http.MethodGet, nil)
-	if err != nil {
-		return createError(fmt.Sprintf("Unable to read integration. IntegrationID: %s",
-			d.Id()), fmt.Sprintf("%v", err))
-	}
-
-	response := ELKIntegrationData{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return createError(fmt.Sprintf("Unable to unmarshall JSON"), fmt.Sprintf("%v", err))
-	}
-	log.Printf("[DEBUG] Response body (unmarshalled): %#v", response)
-
-	d.Set("name", response.Name)
-	d.Set("kibana_url", response.KibanaURL)
-	d.Set("es_url", response.ESURL)
-
-	log.Printf("[DEBUG] End resourceIntegrationELKRead")
-
-	return diag.Diagnostics{}
-}
-
-func resourceIntegrationELKUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationELKUpdate")
-	c := m.(*client.Client)
-
-	resourceData := getELKIntegrationDataFromResource(c, d)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
-
-	if _, err := c.DoRequest(url, http.MethodPut, resourceData); err != nil {
-		return createError("Unable to update integration", fmt.Sprintf("%v", err))
-	}
-
-	log.Printf("[DEBUG] End resourceIntegrationELKUpdate")
-
-	return resourceIntegrationELKRead(ctx, d, m)
-}
-
-func resourceIntegrationELKDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationELKDelete")
-	c := m.(*client.Client)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/elk/%s", c.ControlPlane, d.Id())
-
-	if _, err := c.DoRequest(url, http.MethodDelete, nil); err != nil {
-		return createError("Unable to delete integration", fmt.Sprintf("%v", err))
-	}
-
-	log.Printf("[DEBUG] End resourceIntegrationELKDelete")
-
-	return diag.Diagnostics{}
-}
-
-func getELKIntegrationDataFromResource(c *client.Client, d *schema.ResourceData) ELKIntegrationData {
-	return ELKIntegrationData{
-		ID:        d.Id(),
-		Name:      d.Get("name").(string),
-		KibanaURL: d.Get("kibana_url").(string),
-		ESURL:     d.Get("es_url").(string),
 	}
 }
