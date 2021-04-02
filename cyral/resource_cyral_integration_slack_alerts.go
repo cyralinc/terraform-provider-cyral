0a97307defb442b9ad65370406fcb151ead22030
root <jsaffran@cyral.com>
2021-04-02 17:42:18 +0000

    refactoring slack alerts integration

--- cyral/resource_cyral_integration_slack_alerts.go
+++ cyral/resource_cyral_integration_slack_alerts.go
@@ -1,14 +1,10 @@
 package cyral
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/cyralinc/terraform-provider-cyral/client"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -16,17 +12,73 @@ type CreateSlackAlertsIntegrationResponse struct {
 	ID string `json:"id"`
 }
 
+func (response CreateSlackAlertsIntegrationResponse) WriteResourceData(d *schema.ResourceData) {
+	d.SetId(response.ID)
+}
+
+func (response *CreateSlackAlertsIntegrationResponse) ReadResourceData(d *schema.ResourceData) {
+	response.ID = d.Id()
+}
+
 type SlackAlertsIntegrationData struct {
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
 
+func (data SlackAlertsIntegrationData) WriteResourceData(d *schema.ResourceData) {
+	d.Set("name", data.Name)
+	d.Set("url", data.URL)
+}
+
+func (data *SlackAlertsIntegrationData) ReadResourceData(d *schema.ResourceData) {
+	data.Name = d.Get("name").(string)
+	data.URL = d.Get("url").(string)
+}
+
+var CreateSlackAlertsFunctionConfig = FunctionConfig{
+	Name:       "SlackAlertsResourceCreate",
+	HttpMethod: http.MethodPost,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/notifications/slack", c.ControlPlane)
+	},
+	ResourceData:       &SlackAlertsIntegrationData{},
+	ResponseData:       &CreateSlackAlertsIntegrationResponse{},
+	ReadFunctionConfig: &ReadSlackAlertsFunctionConfig,
+}
+
+var ReadSlackAlertsFunctionConfig = FunctionConfig{
+	Name:       "SlackAlertsResourceRead",
+	HttpMethod: http.MethodGet,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
+	},
+	ResponseData: &SlackAlertsIntegrationData{},
+}
+
+var UpdateSlackAlertsFunctionConfig = FunctionConfig{
+	Name:       "SlackAlertsResourceUpdate",
+	HttpMethod: http.MethodPut,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
+	},
+	ResourceData:       &SlackAlertsIntegrationData{},
+	ReadFunctionConfig: &ReadSlackAlertsFunctionConfig,
+}
+
+var DeleteSlackAlertsFunctionConfig = FunctionConfig{
+	Name:       "SlackAlertsResourceDelete",
+	HttpMethod: http.MethodDelete,
+	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
+		return fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
+	},
+}
+
 func resourceIntegrationSlackAlerts() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceIntegrationSlackAlertsCreate,
-		ReadContext:   resourceIntegrationSlackAlertsRead,
-		UpdateContext: resourceIntegrationSlackAlertsUpdate,
-		DeleteContext: resourceIntegrationSlackAlertsDelete,
+		CreateContext: CreateSlackAlertsFunctionConfig.Create,
+		ReadContext:   ReadSlackAlertsFunctionConfig.Read,
+		UpdateContext: UpdateSlackAlertsFunctionConfig.Update,
+		DeleteContext: DeleteSlackAlertsFunctionConfig.Delete,
 
 		Schema: map[string]*schema.Schema{
 			"name": {
@@ -42,96 +94,5 @@ func resourceIntegrationSlackAlerts() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
-	}
-}
-
-func resourceIntegrationSlackAlertsCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationSlackAlertsCreate")
-	c := m.(*client.Client)
-
-	resourceData := getSlackAlertsIntegrationDataFromResource(c, d)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/notifications/slack", c.ControlPlane)
-
-	body, err := c.DoRequest(url, http.MethodPost, resourceData)
-	if err != nil {
-		return createError("Unable to create integration", fmt.Sprintf("%v", err))
-	}
-
-	response := CreateSlackAlertsIntegrationResponse{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return createError("Unable to unmarshall JSON", fmt.Sprintf("%v", err))
-	}
-	log.Printf("[DEBUG] Response body (unmarshalled): %#v", response)
-
-	d.SetId(response.ID)
-
-	log.Printf("[DEBUG] End resourceIntegrationSlackAlertsCreate")
-
-	return resourceIntegrationSlackAlertsRead(ctx, d, m)
-}
-
-func resourceIntegrationSlackAlertsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationSlackAlertsRead")
-	c := m.(*client.Client)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
-
-	body, err := c.DoRequest(url, http.MethodGet, nil)
-	if err != nil {
-		return createError(fmt.Sprintf("Unable to read integration. IntegrationID: %s",
-			d.Id()), fmt.Sprintf("%v", err))
-	}
-
-	response := SlackAlertsIntegrationData{}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return createError(fmt.Sprintf("Unable to unmarshall JSON"), fmt.Sprintf("%v", err))
-	}
-	log.Printf("[DEBUG] Response body (unmarshalled): %#v", response)
-
-	d.Set("name", response.Name)
-	d.Set("url", response.URL)
-
-	log.Printf("[DEBUG] End resourceIntegrationSlackAlertsRead")
-
-	return diag.Diagnostics{}
-}
-
-func resourceIntegrationSlackAlertsUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationSlackAlertsUpdate")
-	c := m.(*client.Client)
-
-	resourceData := getSlackAlertsIntegrationDataFromResource(c, d)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
-
-	if _, err := c.DoRequest(url, http.MethodPut, resourceData); err != nil {
-		return createError("Unable to update integration", fmt.Sprintf("%v", err))
-	}
-
-	log.Printf("[DEBUG] End resourceIntegrationSlackAlertsUpdate")
-
-	return resourceIntegrationSlackAlertsRead(ctx, d, m)
-}
-
-func resourceIntegrationSlackAlertsDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	log.Printf("[DEBUG] Init resourceIntegrationSlackAlertsDelete")
-	c := m.(*client.Client)
-
-	url := fmt.Sprintf("https://%s/v1/integrations/notifications/slack/%s", c.ControlPlane, d.Id())
-
-	if _, err := c.DoRequest(url, http.MethodDelete, nil); err != nil {
-		return createError("Unable to delete integration", fmt.Sprintf("%v", err))
-	}
-
-	log.Printf("[DEBUG] End resourceIntegrationSlackAlertsDelete")
-
-	return diag.Diagnostics{}
-}
-
-func getSlackAlertsIntegrationDataFromResource(c *client.Client, d *schema.ResourceData) SlackAlertsIntegrationData {
-	return SlackAlertsIntegrationData{
-		Name: d.Get("name").(string),
-		URL:  d.Get("url").(string),
 	}
 }
