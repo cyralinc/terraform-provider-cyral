daeb8d578fd0cf59f3fcacdc2a83c6fffb37d06f
Pedro Tavares de Carvalho <ptcar@pteco.dev>
2021-05-27 13:22:40 -0300

    Add go generate comment to create documentation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: cyral.Provider,
