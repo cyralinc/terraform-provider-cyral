0b2b535dddd8cf2ab3d9812f974b98453fc07cc5
Victor Moraes <vmoraes@cyral.com>
2021-10-05 18:31:17 -0300

    Updates SAML integration resource

--- cyral/resource_cyral_integration_saml.go
+++ cyral/resource_cyral_integration_saml.go
@@ -1,6 +1,7 @@
 package cyral
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -8,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func resourceIntegrationSAML(provider string) *schema.Resource {
+func resourceIntegrationSAML() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: CreateResource(
 			ResourceOperationConfig{
@@ -17,7 +18,7 @@ func resourceIntegrationSAML(provider string) *schema.Resource {
 				CreateURL: func(d *schema.ResourceData, c *client.Client) string {
 					return fmt.Sprintf("https://%s/v1/integrations/saml", c.ControlPlane)
 				},
-				ResourceData: &SAMLSetting{IdentityProvider: provider},
+				ResourceData: &SAMLIntegrationData{},
 				ResponseData: &AliasBasedResponse{},
 			}, readSAMLIntegrationConfig,
 		),
@@ -29,7 +30,7 @@ func resourceIntegrationSAML(provider string) *schema.Resource {
 				CreateURL: func(d *schema.ResourceData, c *client.Client) string {
 					return fmt.Sprintf("https://%s/v1/integrations/saml/%s", c.ControlPlane, d.Id())
 				},
-				ResourceData: &SAMLSetting{IdentityProvider: provider},
+				ResourceData: &SAMLIntegrationData{},
 			}, readSAMLIntegrationConfig,
 		),
 		DeleteContext: DeleteResource(
@@ -44,70 +45,176 @@ func resourceIntegrationSAML(provider string) *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"identity_provider": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: client.ValidateIntegrationSAMLIdentityProvider(),
 			},
-			"display_name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Integration name displayed on cyral's UI.",
-			},
-			"single_sign_on_service_url": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"single_logout_service_url": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"signing_certificate": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"disable_validate_signature": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"back_channel_supported": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"disable_want_authn_requests_signed": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"disable_want_assertions_signed": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"want_assertions_encrypted": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"disable_force_authentication": {
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"allowed_clock_skew": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			"ldap_group_attribute": { //Forgerock only
+			"ldap_group_attribute": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Default:  "cn",
 			},
-			"disable_post_binding_response": { //Forgerock only
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"disable_post_binding_authn_request": { //Forgerock only
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
-			"disable_post_binding_logout": { //Forgerock only
-				Type:     schema.TypeBool,
-				Optional: true,
+			"samlp": {
+				Type:     schema.TypeSet,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"alias": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"provider_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"disabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"first_broker_login_flow_alias": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"post_broker_login_flow_alias": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"display_name": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"store_token": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"add_read_token_role_on_create": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"trust_email": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"link_only": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"internal_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"config": {
+							Type:     schema.TypeSet,
+							Required: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"disable_using_jwks_url": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"sync_mode": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"name_id_policy_format": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"principal_type": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"signature_type": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"saml_xml_key_name_tranformer": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"hide_on_login_page": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"back_channel_supported": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_post_binding_response": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_post_binding_authn_request": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_post_binding_logout": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_want_authn_requests_signed": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_want_assertions_signed": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"want_assertions_encrypted": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_force_authentication": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"disable_validate_signature": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"gui_order": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"single_sign_on_service_url": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"single_logout_service_url": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"xml_sig_key_info_key_name_transformer": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"signing_certificate": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"allowed_clock_skew": {
+										Type:     schema.TypeInt,
+										Optional: true,
+									},
+									"saml_metadata_url": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"base_64_saml_metadata_document": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"ldap_group_attribute": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 
@@ -123,40 +230,121 @@ var readSAMLIntegrationConfig = ResourceOperationConfig{
 	CreateURL: func(d *schema.ResourceData, c *client.Client) string {
 		return fmt.Sprintf("https://%s/v1/integrations/saml/%s", c.ControlPlane, d.Id())
 	},
-	ResponseData: &SAMLSetting{},
+	ResponseData: &SAMLIntegrationData{},
 }
 
-func (data SAMLSetting) WriteToSchema(d *schema.ResourceData) {
-	d.Set("identity_provider", data.IdentityProvider)
-	d.Set("display_name", data.Samlp.DisplayName)
-	d.Set("single_sign_on_service_url", data.Samlp.Config.SingleSignOnServiceURL)
-	d.Set("single_logout_service_url", data.Samlp.Config.SingleLogoutServiceURL)
-	d.Set("signing_certificate", data.Samlp.Config.SigningCertificate)
-	d.Set("disable_validate_signature", data.Samlp.Config.DisableValidateSignature)
-	d.Set("back_channel_supported", data.Samlp.Config.BackChannelSupported)
-	d.Set("disable_want_authn_requests_signed", data.Samlp.Config.DisableWantAuthnRequestsSigned)
-	d.Set("disable_want_assertions_signed", data.Samlp.Config.DisableWantAssertionsSigned)
-	d.Set("want_assertions_encrypted", data.Samlp.Config.WantAssertionsEncrypted)
-	d.Set("disable_force_authentication", data.Samlp.Config.DisableForceAuthentication)
-	d.Set("allowed_clock_skew", data.Samlp.Config.AllowedClockSkew)
-}
+func (data SAMLIntegrationData) WriteToSchema(d *schema.ResourceData) {
+	samlSetting := data.SAMLSetting
+	samlp := make([]interface{}, 0, 1)
+	if samlSetting.Samlp != nil {
+		samlpMap := make(map[string]interface{})
+		samlpMap["alias"] = samlSetting.Samlp.Alias
+		samlpMap["provider_id"] = samlSetting.Samlp.ProviderID
+		samlpMap["disabled"] = samlSetting.Samlp.Disabled
+		samlpMap["first_broker_login_flow_alias"] = samlSetting.Samlp.FirstBrokerLoginFlowAlias
+		samlpMap["post_broker_login_flow_alias"] = samlSetting.Samlp.PostBrokerLoginFlowAlias
+		samlpMap["display_name"] = samlSetting.Samlp.DisplayName
+		samlpMap["store_token"] = samlSetting.Samlp.StoreToken
+		samlpMap["add_read_token_role_on_create"] = samlSetting.Samlp.AddReadTokenRoleOnCreate
+		samlpMap["trust_email"] = samlSetting.Samlp.TrustEmail
+		samlpMap["link_only"] = samlSetting.Samlp.LinkOnly
+		samlpMap["internal_id"] = samlSetting.Samlp.InternalID
 
-func (data *SAMLSetting) ReadFromSchema(d *schema.ResourceData) {
-	if data.IdentityProvider == "" {
-		data.IdentityProvider = d.Get("identity_provider").(string)
-		//TODO: validate if exists, since idp is required
+		config := make([]interface{}, 0, 1)
+		if samlSetting.Samlp.Config != nil {
+			configMap := make(map[string]interface{})
+			configMap["disable_using_jwks_url"] = samlSetting.Samlp.Config.DisableUsingJWKSUrl
+			configMap["sync_mode"] = samlSetting.Samlp.Config.SyncMode
+			configMap["name_id_policy_format"] = samlSetting.Samlp.Config.NameIDPolicyFormat
+			configMap["principal_type"] = samlSetting.Samlp.Config.PrincipalType
+			configMap["signature_type"] = samlSetting.Samlp.Config.SignatureType
+			configMap["saml_xml_key_name_tranformer"] = samlSetting.Samlp.Config.SamlXmlKeyNameTranformer
+			configMap["hide_on_login_page"] = samlSetting.Samlp.Config.HideOnLoginPage
+			configMap["back_channel_supported"] = samlSetting.Samlp.Config.BackChannelSupported
+			configMap["disable_post_binding_response"] = samlSetting.Samlp.Config.DisablePostBindingResponse
+			configMap["disable_post_binding_authn_request"] = samlSetting.Samlp.Config.DisablePostBindingAuthnRequest
+			configMap["disable_post_binding_logout"] = samlSetting.Samlp.Config.DisablePostBindingLogout
+			configMap["disable_want_authn_requests_signed"] = samlSetting.Samlp.Config.DisableWantAuthnRequestsSigned
+			configMap["disable_want_assertions_signed"] = samlSetting.Samlp.Config.DisableWantAssertionsSigned
+			configMap["want_assertions_encrypted"] = samlSetting.Samlp.Config.WantAssertionsEncrypted
+			configMap["disable_force_authentication"] = samlSetting.Samlp.Config.DisableForceAuthentication
+			configMap["disable_validate_signature"] = samlSetting.Samlp.Config.DisableValidateSignature
+			configMap["gui_order"] = samlSetting.Samlp.Config.GuiOrder
+			configMap["single_sign_on_service_url"] = samlSetting.Samlp.Config.SingleSignOnServiceURL
+			configMap["single_logout_service_url"] = samlSetting.Samlp.Config.SingleLogoutServiceURL
+			configMap["xml_sig_key_info_key_name_transformer"] = samlSetting.Samlp.Config.XmlSigKeyInfoKeyNameTransformer
+			configMap["signing_certificate"] = samlSetting.Samlp.Config.SigningCertificate
+			configMap["allowed_clock_skew"] = samlSetting.Samlp.Config.AllowedClockSkew
+			configMap["saml_metadata_url"] = samlSetting.Samlp.Config.SamlMetadataURL
+			configMap["base_64_saml_metadata_document"] = samlSetting.Samlp.Config.Base64SamlMetadataDocument
+			configMap["ldap_group_attribute"] = samlSetting.Samlp.Config.LdapGroupAttribute
+			config = append(config, configMap)
+		}
+
+		samlp = append(samlp, samlpMap)
+		samlpMap["config"] = config
 	}
-	data.Samlp.DisplayName = d.Get("display_name").(string)
-	data.Samlp.Config.SingleSignOnServiceURL = d.Get("single_sign_on_service_url").(string)
-	data.Samlp.Config.SingleLogoutServiceURL = d.Get("single_logout_service_url").(string)
-	data.Samlp.Config.SigningCertificate = d.Get("signing_certificate").(string)
-	data.Samlp.Config.DisableValidateSignature = d.Get("disable_validate_signature").(bool)
-	data.Samlp.Config.BackChannelSupported = d.Get("back_channel_supported").(bool)
-	data.Samlp.Config.DisableWantAuthnRequestsSigned = d.Get("disable_want_authn_requests_signed").(bool)
-	data.Samlp.Config.DisableWantAssertionsSigned = d.Get("disable_want_assertions_signed").(bool)
-	data.Samlp.Config.WantAssertionsEncrypted = d.Get("want_assertions_encrypted").(bool)
-	data.Samlp.Config.DisableForceAuthentication = d.Get("disable_force_authentication").(bool)
-	data.Samlp.Config.AllowedClockSkew = d.Get("allowed_clock_skew").(int)
+	d.Set("identity_provider", samlSetting.IdentityProvider)
+	d.Set("ldap_group_attribute", samlSetting.LdapGroupAttribute)
+	d.Set("samlp", samlp)
+}
+
+func (data *SAMLIntegrationData) ReadFromSchema(d *schema.ResourceData) {
+	samlp := new(IdentityProviderConfig)
+	for _, samlpMap := range d.Get("samlp").(*schema.Set).List() {
+		samlpMap := samlpMap.(map[string]interface{})
+		samlp.Alias = samlpMap["alias"].(string)
+		samlp.ProviderID = samlpMap["provider_id"].(string)
+		samlp.Disabled = samlpMap["disabled"].(bool)
+		samlp.FirstBrokerLoginFlowAlias = samlpMap["first_broker_login_flow_alias"].(string)
+		samlp.PostBrokerLoginFlowAlias = samlpMap["post_broker_login_flow_alias"].(string)
+		samlp.DisplayName = samlpMap["display_name"].(string)
+		samlp.StoreToken = samlpMap["store_token"].(bool)
+		samlp.AddReadTokenRoleOnCreate = samlpMap["add_read_token_role_on_create"].(bool)
+		samlp.TrustEmail = samlpMap["trust_email"].(bool)
+		samlp.LinkOnly = samlpMap["link_only"].(bool)
+		samlp.InternalID = samlpMap["internal_id"].(string)
+		config := new(Config)
+		for _, configMap := range samlpMap["config"].(*schema.Set).List() {
+			configMap := configMap.(map[string]interface{})
+			config.DisableUsingJWKSUrl = configMap["disable_using_jwks_url"].(bool)
+			config.SyncMode = configMap["sync_mode"].(string)
+			config.NameIDPolicyFormat = configMap["name_id_policy_format"].(string)
+			config.PrincipalType = configMap["principal_type"].(string)
+			config.SignatureType = configMap["signature_type"].(string)
+			config.SamlXmlKeyNameTranformer = configMap["saml_xml_key_name_tranformer"].(string)
+			config.HideOnLoginPage = configMap["hide_on_login_page"].(bool)
+			config.BackChannelSupported = configMap["back_channel_supported"].(bool)
+			config.DisablePostBindingResponse = configMap["disable_post_binding_response"].(bool)
+			config.DisablePostBindingAuthnRequest = configMap["disable_post_binding_authn_request"].(bool)
+			config.DisablePostBindingLogout = configMap["disable_post_binding_logout"].(bool)
+			config.DisableWantAuthnRequestsSigned = configMap["disable_want_authn_requests_signed"].(bool)
+			config.DisableWantAssertionsSigned = configMap["disable_want_assertions_signed"].(bool)
+			config.WantAssertionsEncrypted = configMap["want_assertions_encrypted"].(bool)
+			config.DisableForceAuthentication = configMap["disable_force_authentication"].(bool)
+			config.DisableValidateSignature = configMap["disable_validate_signature"].(bool)
+			config.GuiOrder = configMap["gui_order"].(string)
+			config.SingleSignOnServiceURL = configMap["single_sign_on_service_url"].(string)
+			config.SingleLogoutServiceURL = configMap["single_logout_service_url"].(string)
+			config.XmlSigKeyInfoKeyNameTransformer = configMap["xml_sig_key_info_key_name_transformer"].(string)
+			config.SigningCertificate = configMap["signing_certificate"].(string)
+			config.AllowedClockSkew = configMap["allowed_clock_skew"].(int)
+			config.SamlMetadataURL = configMap["saml_metadata_url"].(string)
+			config.Base64SamlMetadataDocument = configMap["base_64_saml_metadata_document"].(string)
+			config.LdapGroupAttribute = configMap["ldap_group_attribute"].(string)
+		}
+		samlp.Config = config
+	}
+
+	data.SAMLSetting = &SAMLSetting{
+		IdentityProvider:   d.Get("identity_provider").(string),
+		LdapGroupAttribute: d.Get("ldap_group_attribute").(string),
+		Samlp:              samlp,
+	}
+}
+
+func (resource *SAMLIntegrationData) MarshalJSON() ([]byte, error) {
+	return json.Marshal(resource.SAMLSetting)
 }
 
 type AliasBasedResponse struct {
--- cyral/resource_cyral_integration_saml_test.go
+++ cyral/resource_cyral_integration_saml_test.go
@@ -2,46 +2,61 @@ package cyral
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-var initialSAMLIntegrationConfig SAMLSetting = SAMLSetting{}
-
-var updatedSAMLIntegrationConfig SAMLSetting = SAMLSetting{}
+const (
+	EnvVarSAMLMetadataURL = "SAML_METADATA_URL"
+)
 
 func TestAccSAMLIntegrationResource(t *testing.T) {
-	/* testConfig, testFunc := setupSAMLIntegrationTest(initialSAMLIntegrationConfig)
-	testUpdateConfig, testUpdateFunc := setupSAMLIntegrationTest(updatedSAMLIntegrationConfig)
-
+	//samlDisplayName := "tf-test-saml-integration"
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: providerFactories,
-		PreCheck:          func() { testAccPreCheck(t) },
-		Steps: []resource.TestStep{
-			{
-				Config: testConfig,
-				Check:  testFunc,
-			},
-			{
-				Config: testUpdateConfig,
-				Check:  testUpdateFunc,
-			},
+		PreCheck: func() {
+			if v := os.Getenv(EnvVarSAMLMetadataURL); v == "" {
+				t.Fatalf("%q must be set for TestAccSAMLIntegrationResource acceptance tests",
+					EnvVarSAMLMetadataURL)
+			}
 		},
-	}) */
+		Steps: []resource.TestStep{
+			/* {
+				Config:      testAccRepoConfAnalysisConfig_ErrorRedact(repoName),
+				ExpectError: regexp.MustCompile(`Error running pre-apply refresh: exit status 1`),
+			}, */
+			{
+				Config: testAccSAMLIntegrationConfig_DefaultValues(),
+				Check:  testAccSAMLIntegrationCheck_DefaultValues(),
+			},
+			/* {
+				Config: testAccRepoConfAnalysisConfig_Updated(repoName),
+				Check:  testAccRepoConfAnalysisCheck_Updated(),
+			}, */
+		},
+	})
 }
 
-func setupSAMLIntegrationTest(integrationData SAMLSetting) (string, resource.TestCheckFunc) {
-	configuration := formatSAMLIntegrationDataIntoConfig(integrationData)
-
-	testFunction := resource.ComposeTestCheckFunc()
-
-	return configuration, testFunction
-}
-
-func formatSAMLIntegrationDataIntoConfig(data SAMLSetting) string {
+func testAccSAMLIntegrationConfig_DefaultValues() string {
 	return fmt.Sprintf(`
-	resource "cyral_saml_integration" "saml_integration" {
-	
-	}`)
+	resource "cyral_integration_saml" "test_saml_integration" {
+		identity_provider = "okta"
+		samlp {
+			config {
+				single_sign_on_service_url = "%s"
+			}
+		}
+	}
+	`, os.Getenv(EnvVarSAMLMetadataURL))
+}
+
+func testAccSAMLIntegrationCheck_DefaultValues() resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttr("cyral_integration_saml.test_saml_integration",
+			"identity_provider", "okta"),
+		resource.TestCheckResourceAttr("cyral_integration_saml.test_saml_integration",
+			"samlp.0.config.0.single_sign_on_service_url", os.Getenv(EnvVarSAMLMetadataURL)),
+	)
 }
