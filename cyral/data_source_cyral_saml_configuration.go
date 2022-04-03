e47290dba579b161edfe9e550d741cff7a149839
Wilson de Carvalho <wjunior@cyral.com>
2022-04-02 23:13:48 -0700

    Migrate data-sources to automated docs

--- cyral/data_source_cyral_saml_configuration.go
+++ cyral/data_source_cyral_saml_configuration.go
@@ -19,109 +19,143 @@ var (
 
 func dataSourceSAMLConfiguration() *schema.Resource {
 	return &schema.Resource{
+		Description: "Parses a SAML metadata URL or a Base64 document into a SAML configuration.",
 		ReadContext: dataSourceSAMLConfigurationRead,
 		Schema: map[string]*schema.Schema{
 			"saml_metadata_url": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ExactlyOneOf: metadataTypes,
+				Description: "(Required unless using `base_64_saml_metadata_document`) This is the full SAML metadata URL we " +
+					"should use to parse to a SAML configuration.",
 			},
 			"base_64_saml_metadata_document": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ExactlyOneOf: metadataTypes,
+				Description: "(Required unless using `saml_metadata_url`) This is the full SAML metadata document that should " +
+					"be used to parse a SAML configuration, Base64 encoded.",
 			},
 			"disable_using_jwks_url": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "By default, the jwks URL is used for all SAML connections.",
 			},
 			"sync_mode": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `FORCE` if unset.",
 			},
 			"name_id_policy_format": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified` if unset.",
 			},
 			"principal_type": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `SUBJECT` if unset.",
 			},
 			"signature_type": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `RSA_SHA256` if unset.",
 			},
 			"saml_xml_key_name_tranformer": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `KEY_ID` if unset.",
 			},
 			"hide_on_login_page": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Defaults to `false` if unset.",
 			},
 			"back_channel_supported": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Defaults to `false` if unset.",
 			},
 			"disable_post_binding_response": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Description: "Indicates whether to respond to requests using `HTTP-POST` binding. If `true`, `HTTP-REDIRECT` " +
+					"binding will be used.",
 			},
 			"disable_post_binding_authn_request": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Description: "Indicates whether the AuthnRequest must be sent using `HTTP-POST` binding. If `true`, " +
+					"`HTTP-REDIRECT` binding will be used.",
 			},
 			"disable_post_binding_logout": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Description: "Indicates whether to respond to requests using `HTTP-POST` binding. If `true`, `HTTP-REDIRECT` " +
+					"binding will be used.",
 			},
 			"disable_want_authn_requests_signed": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Indicates whether the identity provider expects a signed AuthnRequest.",
 			},
 			"disable_want_assertions_signed": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Indicates whether the service provider expects a signed Assertion.",
 			},
 			"want_assertions_encrypted": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Indicates whether the service provider expects an encrypted Assertion.",
 			},
 			"disable_force_authentication": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Description: "Indicates whether the identity provider must authenticate the presenter directly rather than rely " +
+					"on a previous security context.",
 			},
 			"disable_validate_signature": {
-				Type:     schema.TypeBool,
-				Computed: true,
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable/Disable signature validation of SAML responses. Highly recommended for minimum security.",
 			},
 			"gui_order": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "GUI order.",
 			},
 			"single_sign_on_service_url": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The URL that must be used to send authentication requests (SAML AuthnRequest).",
 			},
 			"single_logout_service_url": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The URL that must be used to send logout requests.",
 			},
 			"xml_sig_key_info_key_name_transformer": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Defaults to `KEY_ID` if unset.",
 			},
 			"signing_certificate": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The signing certificate used to validate signatures. Required if signature validation is enabled.",
 			},
 			"allowed_clock_skew": {
-				Type:     schema.TypeInt,
-				Computed: true,
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Clock skew in seconds that is tolerated when validating identity provider tokens. Default value is `0`.",
 			},
 			"ldap_group_attribute": {
 				Type:     schema.TypeString,
 				Computed: true,
+				Description: "The type of `LDAP Group RDN` that identifies the name of a group within a DN. For example, if an " +
+					"LDAP DN sent in a SAML assertion is `cn=Everyone`, `ou=groups`, `dc=openam`, `dc=forgerock`, `dc=org` and " +
+					"the `LDAP Group RDN` Type is `cn` Cyral will interpret `Everyone` as the group name.",
 			},
 		},
 	}
