08d123bfad8df87711f1ae127468cbe2e82a4281
Wilson de Carvalho <wjunior@cyral.com>
2022-04-03 01:18:25 -0700

    Fix data-sources docs

--- examples/data-sources/cyral_sidecar_cft_template/data-source.tf
+++ examples/data-sources/cyral_sidecar_cft_template/data-source.tf
@@ -3,7 +3,7 @@ data "cyral_sidecar_cft_template" "some_data_source_name" {
     log_integration_id = SOME_CYRAL_INTEGRATION.SOME_INTEGRATION_NAME.id
     metrics_integration_id = SOME_CYRAL_INTEGRATION.SOME_INTEGRATION_NAME.id
     aws_configuration {
-        publicly_accessible = true|false
+        publicly_accessible = false
         key_name = "some-ec2-key-name"
     }
 }
