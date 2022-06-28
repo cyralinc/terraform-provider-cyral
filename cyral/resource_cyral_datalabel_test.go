76629e7bfc6b2ee5a9e1707f10a902e20eb51783
Alexander Holmquist <48073242+Yowgf@users.noreply.github.com>
2022-06-28 19:19:45 -0300

    ENG-6805: Add resources for per-repository data map (#230)
    * Functional version of repository_datamap resource
    
    * Add acc test for repository_datamap
    
    * Add functional datalabel resource
    
    * Add acc test for datalabel.
    
    All ACC tests are passing.
    
    * Refactoring
    
    * Remove computed attribute id of datalabel resource

---
+++ cyral/resource_cyral_datalabel_test.go
@@ -0,0 +1,70 @@
+package cyral
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func initialDataLabelConfig() *DataLabel {
+	return &DataLabel{
+		Name:        "test-tf-label1",
+		Description: "label1-description",
+		Tags:        []string{"tag1", "tag2"},
+	}
+}
+
+func updateDataLabelConfig() *DataLabel {
+	return &DataLabel{
+		Name:        "test-tf-label2",
+		Description: "label2-description",
+		Tags:        []string{"tag1", "tag2"},
+	}
+}
+
+func TestAccDatalabelResource(t *testing.T) {
+	testInitialConfig, testInitialFunc := setupDatalabelTest(t,
+		initialDataLabelConfig())
+	testUpdateConfig, testUpdateFunc := setupDatalabelTest(t,
+		updateDataLabelConfig())
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testInitialConfig,
+				Check:  testInitialFunc,
+			},
+			{
+				Config: testUpdateConfig,
+				Check:  testUpdateFunc,
+			},
+		},
+	})
+}
+
+func setupDatalabelTest(t *testing.T, dataLabel *DataLabel) (string, resource.TestCheckFunc) {
+	configuration := formatDataLabelIntoConfig(t, dataLabel)
+
+	testFunction := resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttr("cyral_datalabel.test_datalabel",
+			"name", dataLabel.Name),
+		resource.TestCheckResourceAttr("cyral_datalabel.test_datalabel",
+			"description", dataLabel.Description),
+		resource.TestCheckResourceAttr("cyral_datalabel.test_datalabel",
+			"tags.#", "2"),
+	)
+
+	return configuration, testFunction
+}
+
+func formatDataLabelIntoConfig(t *testing.T, dataLabel *DataLabel) string {
+	return fmt.Sprintf(`
+	resource "cyral_datalabel" "test_datalabel" {
+		name  = "%s"
+		description = "%s"
+		tags = [%s]
+	}`, dataLabel.Name, dataLabel.Description,
+		formatAttributes(dataLabel.Tags))
+}
---
+++ cyral/resource_cyral_repository_datamap_test.go
@@ -0,0 +1,176 @@
+package cyral
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	predefinedLabelCCN = "CCN"
+	predefinedLabelSSN = "SSN"
+	testCustomLabel    = "test-tf-custom-label"
+)
+
+func initialDataMapConfig() *DataMap {
+	return &DataMap{
+		Labels: map[string]*DataMapMapping{
+			predefinedLabelCCN: &DataMapMapping{
+				Attributes: []string{
+					"schema1.table1.col1",
+				},
+			},
+			predefinedLabelSSN: &DataMapMapping{
+				Attributes: []string{
+					"schema1.table1.col2",
+				},
+			},
+		},
+	}
+}
+
+func updateDataMapConfig() *DataMap {
+	return &DataMap{
+		Labels: map[string]*DataMapMapping{
+			predefinedLabelSSN: &DataMapMapping{
+				Attributes: []string{
+					"schema1.table1.col1",
+				},
+			},
+		},
+	}
+}
+
+func dataMapConfigWithDataLabel() (*DataMap, *DataLabel) {
+	return &DataMap{
+			Labels: map[string]*DataMapMapping{
+				testCustomLabel: &DataMapMapping{
+					Attributes: []string{
+						"schema1.table1.col1",
+					},
+				},
+			},
+		}, &DataLabel{
+			Name:        testCustomLabel,
+			Description: "custom-label-description",
+			Tags:        []string{"tag1"},
+		}
+}
+
+func TestAccRepositoryDatamapResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			testRepositoryDatamapInitialConfig(t),
+			testRepositoryDatamapUpdateConfig(t),
+			testRepositoryDatamapWithDatalabel(t),
+		},
+	})
+}
+
+func testRepositoryDatamapInitialConfig(t *testing.T) resource.TestStep {
+	config := formatDataMapIntoConfig(t, initialDataMapConfig())
+	check := setupRepositoryDatamapTestFunc(t, initialDataMapConfig())
+	return resource.TestStep{Config: config, Check: check}
+}
+
+func testRepositoryDatamapUpdateConfig(t *testing.T) resource.TestStep {
+	config := formatDataMapIntoConfig(t, updateDataMapConfig())
+	check := setupRepositoryDatamapTestFunc(t, updateDataMapConfig())
+	return resource.TestStep{Config: config, Check: check}
+}
+
+func testRepositoryDatamapWithDatalabel(t *testing.T) resource.TestStep {
+	configDM, configDL := dataMapConfigWithDataLabel()
+	tfConfig := (formatDataMapIntoConfig(t, configDM) +
+		formatDataLabelIntoConfig(t, configDL))
+	check := setupRepositoryDatamapTestFunc(t, configDM)
+	return resource.TestStep{Config: tfConfig, Check: check}
+}
+
+func setupRepositoryDatamapTestFunc(t *testing.T, dataMap *DataMap) resource.TestCheckFunc {
+	testFunctions := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrPair(
+			"cyral_repository_datamap.test_repository_datamap", "repo_id",
+			"cyral_repository.test_repository", "id"),
+	}
+
+	require.NotNil(t, dataMap.Labels)
+	idxMapping := 0
+	sortedLabels := dataMapSortedLabels(dataMap)
+	for _, label := range sortedLabels {
+		mapping := dataMap.Labels[label]
+
+		testFunctions = append(testFunctions, resource.TestCheckResourceAttr(
+			"cyral_repository_datamap.test_repository_datamap",
+			fmt.Sprintf("mapping.%d.label", idxMapping), label))
+		testFunctions = append(testFunctions, resource.TestCheckResourceAttr(
+			"cyral_repository_datamap.test_repository_datamap",
+			fmt.Sprintf("mapping.%d.attributes.#", idxMapping),
+			fmt.Sprintf("%d", len(mapping.Attributes))))
+
+		idxMapping++
+	}
+
+	testFunction := resource.ComposeTestCheckFunc(testFunctions...)
+
+	return testFunction
+}
+
+func formatDataMapIntoConfig(t *testing.T, dataMap *DataMap) string {
+	dependsOnStr := ""
+	mappingsStr := ""
+	sortedLabels := dataMapSortedLabels(dataMap)
+	for _, label := range sortedLabels {
+		mapping := dataMap.Labels[label]
+
+		require.NotNil(t, mapping)
+		require.NotNil(t, mapping.Attributes)
+
+		mappingsStr += fmt.Sprintf(`
+		mapping {
+			label = "%s"
+			attributes = [%s]
+		}`, label, formatAttributes(mapping.Attributes))
+
+		if label == testCustomLabel {
+			// If there is a custom label in the configuration, we
+			// need to delete the data map first, otherwise the
+			// label cannot be deleted. The depends_on Terraform
+			// meta-argument forces the right deletion order.
+			dependsOnStr = "depends_on = [cyral_datalabel.test_datalabel]"
+		}
+	}
+	require.NotEmpty(t, mappingsStr)
+
+	config := fmt.Sprintf(`
+	resource "cyral_repository" "test_repository" {
+		type  = "sqlserver"
+		host  = "localhost"
+		port  = 1433
+		name  = "tf-test-sqlserver-1"
+		labels = ["repo-label1", "repo-label2"]
+	}
+
+	resource "cyral_repository_datamap" "test_repository_datamap" {
+		%s
+		repo_id = cyral_repository.test_repository.id
+		%s
+	}`, dependsOnStr, mappingsStr)
+
+	return config
+}
+
+// dataMapSortedLabels exists to allow construction and checking of terraform
+// configurations with the data map following the same order of the mappings.
+func dataMapSortedLabels(dataMap *DataMap) []string {
+	var labels []string
+	for label, _ := range dataMap.Labels {
+		labels = append(labels, label)
+	}
+	sort.Strings(labels)
+	return labels
+}
