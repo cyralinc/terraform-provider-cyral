b670b8beb51ac841ab4ca0b803ed42bc2e3f5ced
yowgf <alexthomasmol@gmail.com>
2022-06-30 22:20:38 -0300

    Add guide examples and templates

---
+++ examples/guides/mongodb_cluster_okta_idp_providers.tf
@@ -0,0 +1,23 @@
+provider "cyral" {
+  # Follow the instructions in the Cyral Terraform Provider page to set up the
+  # credentials:
+  #
+  # * https://registry.terraform.io/providers/cyralinc/cyral/latest/docs
+  client_id     = ""
+  client_secret = ""
+  control_plane = "${local.control_plane}:8000"
+}
+
+# Refer to okta provider documentation:
+#
+# * https://registry.terraform.io/providers/okta/okta/latest/docs
+#
+provider "okta" {
+  org_name  = "dev-123456" # your organization name
+  base_url  = "okta.com" # your organization url
+  api_token = "xxxx"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
