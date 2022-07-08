2a7282634f71a93b81d4c1d3cfc4201d27185fae
yowgf <alexthomasmol@gmail.com>
2022-07-08 16:43:51 -0300

    Add cyral_integration_idp_saml_draft resource

--- cyral/resource_cyral_integration_idp_saml_draft.go
+++ cyral/resource_cyral_integration_idp_saml_draft.go
@@ -5,23 +5,71 @@ import (
 	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"github.com/cyralinc/terraform-provider-cyral/client"
 )
 
-type GenericSAMLDraft struct {
+type CreateGenericSAMLDraftRequest struct {
 	DisplayName              string `json:"displayName"`
-	DisableIdPInitiatedLogin string `json:"disableIdPInitiatedLogin,omitempty"`
+	DisableIdPInitiatedLogin bool   `json:"disableIdpInitiatedLogin"`
 }
 
-func (data GenericSAMLDraft) WriteToSchema(d *schema.ResourceData) error {
-	// TODO
+func (req *CreateGenericSAMLDraftRequest) ReadFromSchema(d *schema.ResourceData) error {
+	req.DisplayName = d.Get("display_name").(string)
+	req.DisableIdPInitiatedLogin = d.Get("disable_idp_initiated_login").(bool)
 	return nil
 }
 
-func (data *GenericSAMLDraft) ReadFromSchema(d *schema.ResourceData) error {
-	// TODO
+type GenericSAMLDraftResponse struct {
+	Draft GenericSAMLDraft `json:"draft"`
+}
+
+func (resp *GenericSAMLDraftResponse) WriteToSchema(d *schema.ResourceData) error {
+	d.SetId(resp.Draft.ID)
+	if err := d.Set("display_name", resp.Draft.DisplayName); err != nil {
+		return err
+	}
+	if err := d.Set("disable_idp_initiated_login", resp.Draft.DisableIdPInitiatedLogin); err != nil {
+		return err
+	}
+	if err := d.Set("sp_metadata", resp.Draft.SPMetadata.XMLDocument); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (resp *GenericSAMLDraftResponse) ReadFromSchema(d *schema.ResourceData) error {
+	resp.Draft.ID = d.Id()
+	resp.Draft.DisplayName = d.Get("display_name").(string)
+	resp.Draft.DisableIdPInitiatedLogin = d.Get("disable_idp_initiated_login").(bool)
+	resp.Draft.SPMetadata = SPMetadata{XMLDocument: d.Get("sp_metadata").(string)}
+	return nil
+}
+
+type GenericSAMLDraft struct {
+	ID                       string `json:"id"`
+	DisplayName              string `json:"displayName"`
+	IdPType                  string `json:"idpType"`
+	DisableIdPInitiatedLogin bool   `json:"disableIdpInitiatedLogin"`
+	SPMetadata               `json:"spMetadata"`
+	RequiredUserAttributes   `json:"requiredUserAttributes"`
+	Completed                bool `json:"completed"`
+}
+
+type SPMetadata struct {
+	XMLDocument string `json:"xmlDocument"`
+}
+
+type RequiredUserAttributes struct {
+	FirstName UserAttribute `json:"firstName"`
+	LastName  UserAttribute `json:"lastName"`
+	Email     UserAttribute `json:"email"`
+	Groups    UserAttribute `json:"groups"`
+}
+
+type UserAttribute struct {
+	Name string `json:"name"`
 }
 
 func CreateSAMLDraftConfig() ResourceOperationConfig {
@@ -29,9 +77,10 @@ func CreateSAMLDraftConfig() ResourceOperationConfig {
 		Name:       "SAMLDraftResourceCreate",
 		HttpMethod: http.MethodPost,
 		CreateURL: func(d *schema.ResourceData, c *client.Client) string {
-			return fmt.Sprintf("https://%s/v1/generic-saml/drafts", c.ControlPlane)
+			return fmt.Sprintf("https://%s/v1/integrations/generic-saml/drafts", c.ControlPlane)
 		},
-		NewResponseData: func() ResponseData { return &GenericSAMLDraft{} },
+		NewResourceData: func() ResourceData { return &CreateGenericSAMLDraftRequest{} },
+		NewResponseData: func() ResponseData { return &GenericSAMLDraftResponse{} },
 	}
 }
 
@@ -40,51 +89,41 @@ func ReadSAMLDraftConfig() ResourceOperationConfig {
 		Name:       "SAMLDraftResourceRead",
 		HttpMethod: http.MethodGet,
 		CreateURL: func(d *schema.ResourceData, c *client.Client) string {
-			return fmt.Sprintf("https://%s/v1/generic-saml/drafts/%s", c.ControlPlane, d.Id())
+			return fmt.Sprintf("https://%s/v1/integrations/generic-saml/drafts/%s", c.ControlPlane, d.Id())
 		},
-		NewResponseData: func() ResponseData { return &GenericSAMLDraft{} },
+		NewResponseData: func() ResponseData { return &GenericSAMLDraftResponse{} },
 	}
 }
-
-// func UpdateSAMLDraftConfig() ResourceOperationConfig {
-// 	return ResourceOperationConfig{
-// 		Name:       "SAMLDraftResourceUpdate",
-// 		HttpMethod: http.MethodPost,
-// 		CreateURL: func(d *schema.ResourceData, c *client.Client) string {
-// 			return fmt.Sprintf("https://%s/v1/generic-saml/drafts/%s", c.ControlPlane, d.Id())
-// 		},
-// 		NewResponseData: func() ResponseData { return &GenericSAMLDraft{} },
-// 	}
-// }
 
 func DeleteSAMLDraftConfig() ResourceOperationConfig {
 	return ResourceOperationConfig{
 		Name:       "SAMLDraftResourceDelete",
 		HttpMethod: http.MethodDelete,
 		CreateURL: func(d *schema.ResourceData, c *client.Client) string {
-			return fmt.Sprintf("https://%s/v1/generic-saml/drafts/%s", c.ControlPlane, d.Id())
+			return fmt.Sprintf("https://%s/v1/integrations/generic-saml/drafts/%s", c.ControlPlane, d.Id())
 		},
 	}
 }
 
-func resourceIntegrationSAMLDraft() *schema.Resource {
+func resourceIntegrationIdPSAMLDraft() *schema.Resource {
 	return &schema.Resource{
 		Description: "Manages generic SAML integration drafts.",
 		CreateContext: CreateResource(
 			CreateSAMLDraftConfig(),
 			ReadSAMLDraftConfig(),
 		),
-		ReadContext: ReadResource(ReadSAMLDraftConfig()),
-		//		UpdateContext: UpdateResource(UpdateSAMLDraftConfig()),
+		ReadContext:   ReadResource(ReadSAMLDraftConfig()),
 		DeleteContext: DeleteResource(DeleteSAMLDraftConfig()),
 		Schema: map[string]*schema.Schema{
-			// All of the input arguments force a recreation of the
-			// draft, because the API does not support updates.
+			// All of the input arguments must force recreation of
+			// the resource, because the API does not support
+			// updates.
 			"display_name": {
-				Description: "Display name used in the Cyral control plane.",
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
+				Description:  "Display name used in the Cyral control plane.",
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				ForceNew:     true,
 			},
 			"disable_idp_initiated_login": {
 				Description: "Whether or not IdP-Initiated login should be disabled for this generic SAML integration draft. Defaults to false.",
@@ -93,8 +132,13 @@ func resourceIntegrationSAMLDraft() *schema.Resource {
 				ForceNew:    true,
 				Default:     false,
 			},
-			"saml_metadata_document": {
+			"sp_metadata": {
 				Description: "The SP Metadata document describing the Cyral service provider for this integration.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"id": {
+				Description: "ID of this resource in the Cyral environment.",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
