68fec8b8ac9fdf22b7ce76ab203c820750dbdfa0
yowgf <alexthomasmol@gmail.com>
2022-07-29 23:34:58 -0300

    Simplify cyral_sidecar_credentials test

--- cyral/resource_cyral_sidecar_credentials_test.go
+++ cyral/resource_cyral_sidecar_credentials_test.go
@@ -4,23 +4,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
-
-func testSidecarCredentialsImportStateCheck(states []*terraform.InstanceState) error {
-	for _, state := range states {
-		id := state.ID
-		attributes := state.Attributes
-		if client_id, ok := attributes["client_id"]; !ok {
-			return importErrorf(id, "client ID not found in state attributes")
-		} else {
-			if id != client_id {
-				return importErrorf(id, "expected client ID to be equal to ID")
-			}
-		}
-	}
-	return nil
-}
 
 func TestAccSidecarCredentialsResource(t *testing.T) {
 	testConfig, testFunc := setupSidecarCredentialsTest()
@@ -33,14 +17,10 @@ func TestAccSidecarCredentialsResource(t *testing.T) {
 				Check:  testFunc,
 			},
 			{
-				// The check for sidecar credentials needs to be
-				// manual, because ImportStateVerify will expect
-				// that client_secret is set, but the GET API
-				// does not return the sidecar account's client
-				// secret for security reasons.
-				ImportState:      true,
-				ImportStateCheck: testSidecarCredentialsImportStateCheck,
-				ResourceName:     "cyral_sidecar_credentials.test_sidecar_credentials",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"client_secret"},
+				ResourceName:            "cyral_sidecar_credentials.test_sidecar_credentials",
 			},
 		},
 	})
