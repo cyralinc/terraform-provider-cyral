94cc2eef99cff39cf7ef954c1c8ac2e09dede4a8
yowgf <alexthomasmol@gmail.com>
2022-08-01 19:54:51 -0300

    Address PR comment

--- cyral/resource_cyral_sidecar.go
+++ cyral/resource_cyral_sidecar.go
@@ -101,17 +101,14 @@ func resourceSidecar() *schema.Resource {
 				Optional:    true,
 			},
 			"bypass_mode": {
-				Description: "This argument lets you specify how to handle the connection in the event of an error in the sidecar during a user’s session. Valid modes are: `never`, `failover`, `always`. Defaults to `never`. If `never` is specified and there is an error in the sidecar, connections to bound repositories will fail. If `failover` is specified, the sidecar will run in [resiliency mode](https://cyral.com/docs/sidecars/sidecar-manage#resilient-mode-of-sidecar-operation). If `always` is specified, the sidecar will run in [passthrough mode](https://cyral.com/docs/sidecars/sidecar-manage#passthrough-mode).",
+				Description: "This argument lets you specify how to handle the connection in the event of an error in the sidecar during a user’s session. Valid modes are: `always`, `failover` or `never`. Defaults to `failover`. If `always` is specified, the sidecar will run in [passthrough mode](https://cyral.com/docs/sidecars/sidecar-manage#passthrough-mode). If `failover` is specified, the sidecar will run in [resiliency mode](https://cyral.com/docs/sidecars/sidecar-manage#resilient-mode-of-sidecar-operation). If `never` is specified and there is an error in the sidecar, connections to bound repositories will fail.",
 				Type:        schema.TypeString,
 				Optional:    true,
-				// "failover" is the default value the UI
-				// assumes. "never" is the default value the API
-				// assumes.
-				Default: "never",
+				Default:     "failover",
 				ValidateFunc: validation.StringInSlice([]string{
-					"never",
-					"failover",
 					"always",
+					"failover",
+					"never",
 				}, false),
 			},
 			"certificate_bundle_secrets": {
