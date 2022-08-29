f702982955fc1fe0ae99218fea194efa6d1f42c7
yowgf <alexthomasmol@gmail.com>
2022-08-29 18:57:24 -0300

    Remove commented tests

--- cyral/data_source_cyral_role_test.go
+++ cyral/data_source_cyral_role_test.go
@@ -7,26 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-// TODO: currently, we just test that the configs are valid. We need to add ACC
-// tests for a full scenario, containing roles, IdPs and user groups (see
-// resources `cyral_role` and `cyral_role_sso_groups`. -aholmquist 2022-08-05
-/*
-func roleDataSourceTestUserGroupsAndRoleNames() ([]*UserGroup, []string) {
-	return []*UserGroup{
-		{
-			Name:        "tfprov-test-user-group-1",
-			Description: "description-1",
-		},
-		{
-			Name:        "tfprov-test-user-group-2",
-			Description: "description-2",
-		},
-	}, []string{
-		"tfprov-test-role-1",
-		"tfprov-test-role-2",
-	}
-}
-*/
+// TODO: More tests -aholmquist 2022-08-29
 
 func TestAccRoleDataSource(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
