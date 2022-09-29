cb20dd4b1937646abdeb5d854de673b4553af6e1
yowgf <alexthomasmol@gmail.com>
2022-09-29 13:40:54 -0300

    Refine most of native credentials guide

--- examples/guides/native_credentials_aws_sm.tf
+++ examples/guides/native_credentials_aws_sm.tf
@@ -2,52 +2,62 @@
 # to a cyral repository is defined here for clarity, but you
 # can define somewhere else.
 locals {
-    database_name = "someDatabaseName"
-    database_credentials = {
-        username = "someUserName"
-        password = "somePassword"
-    }
+  # Replace [TENANT] by your tenant name. Ex: mycompany.app.cyral.com
+  control_plane_host = "[TENANT].app.cyral.com"
+  control_plane_port = 443
+
+  database_name = ""
+  database_credentials = {
+    username = ""
+    password = ""
+  }
 }
 
-# See the Cyral provider documentation for more
-# information on how to initialize it correctly.
 provider "cyral" {
-    control_plane = "mycontrolplane.cyral.com:8000"
+  # Follow the instructions in the Cyral Terraform Provider page to
+  # set up the Control Plane credentials:
+  #
+  # * https://registry.terraform.io/providers/cyralinc/cyral/latest/docs
+  client_id     = ""
+  client_secret = ""
+  control_plane = "${local.control_plane_host}:${local.control_plane_port}"
+}
+
+# See the AWS provider documentation for more information on how to
+# initialize it correctly.
+provider "aws" {
+  # By deploying the secret to the same account and region of your
+  # sidecar and using the name suggested in my_repository_secret, the
+  # sidecar will gain access to the secret automatically.
+  region = "us-east-1"
 }
 
 resource "cyral_repository" "mongodb_repo" {
-    type = "mongodb"
-    host = "mongodb.cyral.com"
-    port = 27017
-    name = "mymongodb"
+  type = "mongodb"
+  host = ""
+  port = 27017
+  name = "mymongodb"
 }
 
-resource "cyral_repository_local_account" "my_repo_account" {
-    repository_id = cyral_repository.mongodb_repo.id
+resource "cyral_repository_user_account" "my_user_account" {
+  repository_id      = cyral_repository.mongodb_repo.id
+  name               = local.database_credentials.username
+  auth_database_name = local.database_name
+  auth_scheme {
     aws_secrets_manager {
-        database_name = local.database_name
-        local_account = local.database_credentials.username
-        secret_arn = aws_secretsmanager_secret.my_repository_secret.arn
+      secret_arn = aws_secretsmanager_secret.my_repository_secret.arn
     }
-}
-
-# See the AWS provider documentation for more
-# information on how to initialize it correctly.
-provider "aws" {
-    # By deploying the secret to the same account and region of your
-    # sidecar and using the name suggested in my_repository_secret,
-    # the sidecar will gain access to the secret automatically.
-    region = "us-east-1"
+  }
 }
 
 resource "aws_secretsmanager_secret" "my_repository_secret" {
-    name = join("", [
-      "/cyral/dbsecrets/",
-      cyral_repository.mongodb_repo.id
-    ])
+  name = join("", [
+    "/cyral/dbsecrets/",
+    cyral_repository.mongodb_repo.id
+  ])
 }
 
 resource "aws_secretsmanager_secret_version" "my_repository_secret_version" {
-    secret_id     = aws_secretsmanager_secret.my_repository_secret.id
-    secret_string = jsonencode(local.database_credentials)
+  secret_id     = aws_secretsmanager_secret.my_repository_secret.id
+  secret_string = jsonencode(local.database_credentials)
 }
