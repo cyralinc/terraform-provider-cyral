4ac68483abe73b0188c5bcad06f10583dca9b2b3
yowgf <alexthomasmol@gmail.com>
2022-11-04 13:11:04 -0300

    Update docs

--- examples/resources/cyral_repository_conf_analysis/resource.tf
+++ examples/resources/cyral_repository_conf_analysis/resource.tf
@@ -7,7 +7,7 @@ resource "cyral_repository_conf_analysis" "all_conf_analysis_enabled" {
   disable_pre_configured_alerts = false
   block_on_violation = true
   disable_filter_analysis = false
-  rewrite_on_violation = true
+  enable_dataset_rewrites = true
   enable_data_masking = true
   comment_annotation_groups = [ "identity" ]
   log_groups = [ "everything" ]
@@ -21,7 +21,7 @@ resource "cyral_repository_conf_analysis" "all_conf_analysis_disabled" {
   disable_pre_configured_alerts = true
   block_on_violation = false
   disable_filter_analysis = true
-  rewrite_on_violation = false
+  enable_dataset_rewrites = false
   enable_data_masking = false
   comment_annotation_groups = []
   log_groups = []
