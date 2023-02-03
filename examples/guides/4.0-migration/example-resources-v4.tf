5cd310f67ddaeb135f2f8939ad427093cd21b5a3
Wilson de Carvalho <796900+wcmjunior@users.noreply.github.com>
2023-02-03 13:35:27 -0800

    Organize 'examples' folder

---
+++ examples/guides/4.0-migration/example-resources-v4.tf
@@ -0,0 +1,32 @@
+resource "cyral_repository" "mongo_repo" {
+    name = "mongo-repo"
+    type = "mongodb"
+    repo_node {
+    host = "mongodb.cyral.com"
+    port = 27017
+    }
+}
+
+resource "cyral_repository_binding" "binding" {
+    sidecar_id    = cyral_sidecar.sidecar.id
+    repository_id = cyral_repository.mongo_repo.id
+    enabled       = true
+    listener_binding {
+    listener_id = cyral_sidecar_listener.listener.listener_id
+    node_index  = 0
+    }
+}
+
+resource "cyral_sidecar_listener" "listener" {
+    sidecar_id = cyral_sidecar.sidecar.id
+    repo_types = ["mongodb"]
+    network_address {
+    port = 27020
+    }
+}
+
+resource "cyral_repository_access_gateway" "access_gateway" {
+    repository_id = cyral_repository.mongo_repo.id
+    sidecar_id    = cyral_sidecar.sidecar.id
+    binding_id    = cyral_repository_binding.binding.binding_id
+}
