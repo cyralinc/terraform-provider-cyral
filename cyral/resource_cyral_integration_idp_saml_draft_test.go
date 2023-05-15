6c9880953be078345e8b73fc3c6013a1e196c8e3
gabriel <gabpaivapereira@gmail.com>
2023-05-15 19:03:06 -0300

    Change to check in tests

--- cyral/resource_cyral_integration_idp_saml_draft_test.go
+++ cyral/resource_cyral_integration_idp_saml_draft_test.go
@@ -124,24 +124,16 @@ func setupIntegrationIdPSAMLDraftTest(t *testing.T, draft *GenericSAMLDraft, res
 
 	// checking SPMetadata content
 	checkFuncs = append(checkFuncs, []resource.TestCheckFunc{
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.xml_document",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.entity_id",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.url",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.single_logout_url",
-			nonEmptyRegex),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.xml_document"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.entity_id"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.url"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.single_logout_url"),
 	}...)
 	checkFuncs = append(checkFuncs, []resource.TestCheckFunc{
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.0.url",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.0.index",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.1.url",
-			nonEmptyRegex),
-		resource.TestMatchResourceAttr(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.1.index",
-			nonEmptyRegex),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.0.url"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.0.index"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.1.url"),
+		resource.TestCheckResourceAttrSet(resourceFullName, "service_provider_metadata.0.assertion_consumer_services.1.index"),
 	}...)
 
 	return config, resource.ComposeTestCheckFunc(checkFuncs...)
