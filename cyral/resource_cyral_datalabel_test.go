a0918f5215cf099fdcfb6323490ee389024283df
Victor Moraes <vmoraes@cyral.com>
2023-08-10 20:11:43 -0300

    Fix tests

--- cyral/resource_cyral_datalabel_test.go
+++ cyral/resource_cyral_datalabel_test.go
@@ -97,23 +97,30 @@ func datalabelConfigResourceFullName(resName string) string {
 }
 
 func formatDataLabelIntoConfig(resName string, dataLabel *DataLabel) string {
+	var classificationRuleConfig string
+	if dataLabel.ClassificationRule != nil {
+		classificationRuleConfig = fmt.Sprintf(`
+		classification_rule {
+			rule_type = "%s"
+			rule_code = "%s"
+			rule_status = "%s"
+		}`,
+			dataLabel.ClassificationRule.RuleType,
+			dataLabel.ClassificationRule.RuleCode,
+			dataLabel.ClassificationRule.RuleStatus,
+		)
+	}
 	return fmt.Sprintf(`
 	resource "cyral_datalabel" "%s" {
 		name  = "%s"
 		description = "%s"
 		tags = %s
-		classification_rule {
-			rule_type = "%s"
-			rule_code = "%s"
-			rule_status = "%s"
-		}
+		%s
 	}`,
 		resName,
 		dataLabel.Name,
 		dataLabel.Description,
 		listToStr(dataLabel.Tags),
-		dataLabel.ClassificationRule.RuleType,
-		dataLabel.ClassificationRule.RuleCode,
-		dataLabel.ClassificationRule.RuleStatus,
+		classificationRuleConfig,
 	)
 }
