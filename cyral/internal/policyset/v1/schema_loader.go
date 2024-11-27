fb28fbb5e565591b96f376a6e1aaf294dbc279f4
gengdahlCyral <gengdahl@cyral.com>
2024-11-27 09:36:05 +0100

    Rename policywizardv1 to policysetv1 and improve URL handling

--- cyral/internal/policyset/v1/schema_loader.go
+++ cyral/internal/policyset/v1/schema_loader.go
@@ -1,4 +1,4 @@
-package policywizardv1
+package policysetv1
 
 import "github.com/cyralinc/terraform-provider-cyral/cyral/core"
 
@@ -6,7 +6,7 @@ type packageSchema struct {
 }
 
 func (p *packageSchema) Name() string {
-	return "policywizardv1"
+	return "policysetv1"
 }
 
 func (p *packageSchema) Schemas() []*core.SchemaDescriptor {
